8d6aaa40ef3e33ae63766a020741d0906e11cc8c
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2022-01-20 14:16:04 +0100

    Setup dev shell with cachix + linting tools

--- flake.nix
+++ flake.nix
@@ -1,18 +1,24 @@
 {
   description = "My NixOS and home-manager configurations";
+  nixConfig.bash-prompt = "\[nix-config-dev\]$ ";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-21.11";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     let
       mkNixOS  = import ./lib/mk-nixos.nix;
       system   = "x86_64-linux";
       revision =  nixpkgs.lib.mkIf (self ? rev) self.rev;
-    in {
+      allDevShells = flake-utils.lib.eachDefaultSystem(system: {
+        devShell = (import ./dev-shell.nix { inherit system nixpkgs; });
+      });
+    in nixpkgs.lib.recursiveUpdate allDevShells {
       nixosConfigurations = {
-        vm =  mkNixOS "vm" { inherit nixpkgs system revision; };
+        vm = mkNixOS "vm" { inherit nixpkgs system revision; };
       };
     };
 }
