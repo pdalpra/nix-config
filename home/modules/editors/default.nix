dd37365530ed555eb9562ec8e42fc1719d514121
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2022-07-09 20:16:36 +0200

    Setup VS Code (#15)

--- home/modules/editors/default.nix
+++ home/modules/editors/default.nix
@@ -1,3 +1,4 @@
 [
   ./vim.nix
+  ./vscode.nix
 ]
---
+++ home/modules/editors/vscode.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+
+    extensions = with pkgs.vscode-extensions; [
+      eamodio.gitlens
+      jnoortheen.nix-ide
+      arrterian.nix-env-selector
+      scalameta.metals
+      scala-lang.scala
+      ms-vscode.theme-tomorrowkit
+    ];
+
+    userSettings = {
+      "editor.fontSize" = 16;
+      "editor.fontFamily" = "'Jetbrains Mono', 'monospace', monospace";
+      "editor.fontLigatures" = true;
+      "editor.renderWhitespace" = "boundary";
+      "editor.formatOnPaste" = true;
+      "editor.formatOnSave" = true;
+      "files.autoSave" = "onFocusChange";
+      "explorer.confirmDragAndDrop" = false;
+      "workbench.startupEditor" = "none";
+      "window.zoomLevel" = 2.5;
+      "workbench.colorTheme" = "Tomorrow Night Eighties";
+      "telemetry.telemetryLevel" = "off";
+      "update.showReleaseNotes" = false;
+      "security.workspace.trust.enabled" = false;
+      "git.allowForcePush" = true;
+      "gitlens.showWhatsNewAfterUpgrades" = false;
+      "gitlens.showWelcomeOnInstall" = false;
+      "nix.enableLanguageServer" = true;
+    };
+  };
+}
