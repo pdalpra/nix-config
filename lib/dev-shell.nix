23d536131ce7b56429e1cec9a9adbc2394479a89
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2023-06-04 16:08:36 +0200

    Install treefmt

--- lib/dev-shell.nix
+++ lib/dev-shell.nix
@@ -2,11 +2,18 @@
 
 with pkgs;
 
+let
+  fmtAll = writeShellScriptBin "fmt-all" "treefmt .github .";
+in
 mkShell {
   name = "nix-config-dev";
+
   buildInputs = [
     cachix
     git-secret
+    fmtAll
     statix
+    treefmt
+    yamlfix
   ];
 }
