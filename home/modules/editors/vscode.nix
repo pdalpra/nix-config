3a39f4a3c4c902c0d7367adbad98394d50465202
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2023-09-17 15:01:01 +0200

    Use custom VS Code extension to manage default config (#44)

--- home/modules/editors/vscode.nix
+++ home/modules/editors/vscode.nix
@@ -1,8 +1,25 @@
 { pkgs, ... }:
 
+let
+  myDefaults =
+    pkgs.vscode-utils.buildVscodeExtension {
+      name = "pdalpra.vscode-defaults";
+      vscodeExtUniqueId = "vscode-defaults";
+      vscodeExtName = "My Defaults";
+      vscodeExtPublisher = "pdalpra";
+      version = "0.0.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "pdalpra";
+        repo = "vscode-defaults";
+        rev = "919c61806df97badbfabaa2ee4fcbeb0343e9b27";
+        sha256 = "sha256-p2LKmNuKwXy8FTpTX+SJSLIAFyFqNIXz/FHDsK65a5I=";
+      };
+    };
+in
 {
   programs.vscode = {
     enable = true;
+    package = pkgs.unstable.vscode;
 
     extensions = with pkgs.vscode-extensions; [
       eamodio.gitlens
@@ -11,28 +28,7 @@
       scalameta.metals
       scala-lang.scala
       ms-vscode.theme-tomorrowkit
+      myDefaults
     ];
-
-    userSettings = {
-      "editor.fontSize" = 12;
-      "editor.fontFamily" = "'Jetbrains Mono Nerd Font', 'monospace', monospace";
-      "editor.fontLigatures" = true;
-      "editor.renderWhitespace" = "boundary";
-      "editor.formatOnPaste" = true;
-      "editor.formatOnSave" = true;
-      "files.autoSave" = "onFocusChange";
-      "explorer.confirmDragAndDrop" = false;
-      "workbench.startupEditor" = "none";
-      "window.zoomLevel" = 2.5;
-      "workbench.colorTheme" = "Tomorrow Night Eighties";
-      "telemetry.telemetryLevel" = "off";
-      "update.showReleaseNotes" = false;
-      "security.workspace.trust.enabled" = false;
-      "git.allowForcePush" = true;
-      "gitlens.showWhatsNewAfterUpgrades" = false;
-      "gitlens.showWelcomeOnInstall" = false;
-      "nix.enableLanguageServer" = true;
-      "update.mode" = "none";
-    };
   };
 }
