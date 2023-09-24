2a46468608c339ca6c03ba968d3cafce8ff19680
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2023-09-24 15:04:57 +0200

    Tweak formatting in XMonad bindings definition

--- home/modules/wm/xmonad-config/lib/Bindings.hs
+++ home/modules/wm/xmonad-config/lib/Bindings.hs
@@ -29,7 +29,7 @@ setup c =
     query = title =? helpTitle
     setHelpGeometry = query --> doRectFloat (W.RationalRect (1 % 3) (1 % 4) (1 % 3) (1 % 2))
     closeHelpOnFocusLost =
-      withUnfocused $ \w -> runQuery query w --> withDisplay (io . void . (`killClient` w))
+      withUnfocused $ \w -> runQuery query w --> withDisplay (io . void . flip killClient w)
 
 
 showKeybindings :: String -> [((KeyMask, KeySym), NamedAction)] -> NamedAction
@@ -52,40 +52,40 @@ myKeys c =
         ]
     , keySet
         "Layouts"
-        [ key "Next layout"          (modm, xK_space)               $ sendMessage NextLayout
+        [ key "Next layout"          (modm,               xK_space) $ sendMessage NextLayout
         , key "Reset to base layout" (modm .|. shiftMask, xK_space) $ setLayout c.layoutHook
-        , key "Toggle fullscreen"    (modm, xK_f) $ sendMessage     $ Toggle NBFULL
+        , key "Toggle fullscreen"    (modm,               xK_f)     $ sendMessage $ Toggle NBFULL
         ]
     , keySet
         "System"
         [ key "Power menu"             (modm .|. shiftMask, xK_q) $ spawn "rofi -show pm"
         , key "Lock screen"            (modm .|. shiftMask, xK_l) $ spawn "xset s activate"
-        , key "Screenshot (selection)" (modm, xK_s)               $ spawn "scrot -s -F $HOME/Pictures/screenshots/%d-%m-%Y_%H:%M.png"
+        , key "Screenshot (selection)" (modm,               xK_s) $ spawn "scrot -s -F $HOME/Pictures/screenshots/%d-%m-%Y_%H:%M.png"
         , key "Screenshot (full)"      (modm .|. shiftMask, xK_s) $ spawn "scrot -F $HOME/Pictures/screenshots/%d-%m-%Y_%H:%M.png"
-        , key "Restart XMonad"         (modm, xK_q)               $ spawn "xmonad --restart"
+        , key "Restart XMonad"         (modm,               xK_q) $ spawn "xmonad --restart"
         ]
     , keySet
         "Windows => switch"
-        [ key "Previous"      (modm .|. shiftMask, xK_Left)              $ windows W.focusUp
-        , key "Next"          (modm .|. shiftMask, xK_Right)             $ windows W.focusDown
-        , key "Focus main"    (modm, xK_m)                               $ windows W.focusMaster
-        , key "Switcher"      (modm .|. shiftMask, xK_Tab)               $ spawn "rofi -show window"
-        , key "Close focused" (modm .|. shiftMask, xK_c)                 kill
-        , key "Close any"     (modm .|. controlMask .|. shiftMask, xK_c) $ spawn "xkill"
+        [ key "Previous"      (modm .|. shiftMask,                 xK_Left)  $ windows W.focusUp
+        , key "Next"          (modm .|. shiftMask,                 xK_Right) $ windows W.focusDown
+        , key "Focus main"    (modm,                               xK_m)     $ windows W.focusMaster
+        , key "Switcher"      (modm .|. shiftMask,                 xK_Tab)   $ spawn "rofi -show window"
+        , key "Close focused" (modm .|. shiftMask,                 xK_c)     kill
+        , key "Close any"     (modm .|. controlMask .|. shiftMask, xK_c)     $ spawn "xkill"
         ]
     , keySet
         "Windows => move"
-        [ key "Swap with main"               (modm, xK_Return)              $ windows W.swapMaster
-        , key "Swap with next"               (modm .|. shiftMask, xK_Down)  $ windows W.swapDown
-        , key "Swap with previous"           (modm .|. shiftMask, xK_Up)    $ windows W.swapUp
-        , key "Tile focused floating window" (modm, xK_t) $ withFocused     $ windows . W.sink
+        [ key "Swap with main"               (modm,               xK_Return) $ windows W.swapMaster
+        , key "Swap with next"               (modm .|. shiftMask, xK_Down)   $ windows W.swapDown
+        , key "Swap with previous"           (modm .|. shiftMask, xK_Up)     $ windows W.swapUp
+        , key "Tile focused floating window" (modm,               xK_t)      $ withFocused $ windows . W.sink
         ]
     , keySet
         "Windows => resize"
-        [ key "Shrink main area"             (modm, xK_minus)               $ sendMessage Shrink
-        , key "Expand main area"             (modm .|. shiftMask, xK_equal) $ sendMessage Expand
-        , key "Add window to main area"      (modm, xK_comma)               $ sendMessage $ IncMasterN 1
-        , key "Remove window from main area" (modm, xK_period)              $ sendMessage $ IncMasterN (-1)
+        [ key "Shrink main area"             (modm,               xK_minus)  $ sendMessage Shrink
+        , key "Expand main area"             (modm .|. shiftMask, xK_equal)  $ sendMessage Expand
+        , key "Add window to main area"      (modm,               xK_comma)  $ sendMessage $ IncMasterN 1
+        , key "Remove window from main area" (modm,               xK_period) $ sendMessage $ IncMasterN (-1)
         ]
     , keySet "Workspaces" switchOrMoveOnWorkspaces
     , keySet "Screens"    switchOrMoveOnScreens
