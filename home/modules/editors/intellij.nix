f30cef6be1e2aee1e8dd6c94ec7f31222aa60cd0
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2023-10-04 00:19:17 +0200

    Manage IntelliJ with Jetbrains Toolbox (#50)

--- home/modules/editors/intellij.nix
+++ home/modules/editors/intellij.nix
@@ -1,12 +1,7 @@
 { pkgs, ... }:
 
-let
-  ideaPackage = pkgs.unstable.jetbrains.idea-ultimate;
-  ideaAlias = pkgs.writeShellScriptBin "idea" "${ideaPackage}/bin/idea-ultimate";
-in
 {
-  home.packages = [
-    ideaPackage
-    ideaAlias
+  home.packages = with pkgs; [
+    unstable.jetbrains-toolbox
   ];
 }
