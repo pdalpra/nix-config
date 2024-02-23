743bcdf0b2b593739ae42d13e506f0101bd4b352
Pierre Dal-Pra <p.dalpra@stuart.com>
2024-02-23 23:04:47 +0100

    Setup ragenix

--- lib/dev-shell.nix
+++ lib/dev-shell.nix
@@ -1,4 +1,4 @@
-{ pkgs, unstable }:
+{ pkgs, unstable, agenixBin }:
 
 with pkgs;
 
@@ -9,13 +9,15 @@ let
     ${treefmt}/bin/treefmt --fail-on-change --no-cache ${sourcePaths};
     statix check
   '';
-
+  agenixFile = writeShellScriptBin "agenix-file" "cat $2 | ${agenixBin}/bin/agenix --editor='-' -e $1";
   nixTools = [
     cachix
     git-secret
     nixpkgs-fmt
     unstable.nil
     statix
+    agenixBin
+    agenixFile
   ];
   haskellTools = with haskellPackages; [
     cabal-install
@@ -34,7 +36,6 @@ let
     zlib.dev
   ];
   miscTools = [
-    git-secret
     fmtAll
     lintAll
     yamlfix
