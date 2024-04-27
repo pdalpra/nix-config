cf221bc145b1e94825131960561f67188437ba5c
Pierre Dal-Pra <dalpra.pierre@proton.me>
2024-04-27 21:29:53 +0200

    Improve xsecurelock config

--- home/modules/wm/screenlocker.nix
+++ home/modules/wm/screenlocker.nix
@@ -3,7 +3,8 @@ let
   xsecurelock =
     pkgs.xsecurelock.overrideAttrs (previous: {
       buildInputs = with pkgs;
-        (lib.lists.subtractLists [ apacheHttpd pamtester ] previous.buildInputs) ++ [ makeWrapper ];
+        (lib.lists.subtractLists [ apacheHttpd pamtester ] previous.buildInputs)
+        ++ [ makeWrapper ];
 
       postInstall = (previous.postInstall or "") + ''
         wrapProgram "$out/bin/xsecurelock" \
@@ -12,6 +13,9 @@ let
         --set XSECURELOCK_NO_COMPOSITE 1 \
         --set XSECURELOCK_FONT "Berkeley Mono:size=20:style=Medium,Regular" \
         --set XSECURELOCK_SHOW_DATETIME 1 \
+        --set XSECURELOCK_BLANK_DPMS_STATE suspend \
+        --set XSECURELOCK_SHOW_HOSTNAME 0 \
+        --set XSECURELOCK_SAVER_STOP_ON_BLANK 1 \
         --set XSECURELOCK_DATETIME_FORMAT "%d/%m/%Y - %H:%M:%S"
       '';
     });
@@ -28,6 +32,7 @@ in
     inactiveInterval = 10;
     lockCmd = "${xsecurelock}/bin/xsecurelock";
     xss-lock.extraOptions = [
+      "-l"
       "-n"
       "${xsecurelock}/libexec/xsecurelock/dimmer"
     ];
