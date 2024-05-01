9f855d1bdb957aedbd2ebe35d35a65eecf79edec
Pierre Dal-Pra <dalpra.pierre@proton.me>
2024-05-01 14:28:46 +0200

    Configure Whatsapp (#106)

--- home/modules/web/chat.nix
+++ home/modules/web/chat.nix
@@ -7,4 +7,19 @@
     unstable.slack
     zoom-us
   ];
+
+  xdg.configFile."whatsapp-for-linux/settings.conf".text =
+    ''
+      [web]
+      allow-permissions=true
+      hw-accel=1
+      min-font-size=16
+
+      [general]
+      notification-sounds=true
+      close-to-tray=true
+
+      [appearance]
+      prefer-dark-theme=true
+    '';
 }
