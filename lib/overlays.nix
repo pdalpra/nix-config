792103aaeadbb22d5910bb44bef65d585b0c3f04
Pierre Dal-Pra <dalpra.pierre@proton.me>
2024-05-11 23:25:46 +0200

    Add an overlay to setup Steam + gamescope + mangohud (#110)

--- lib/overlays.nix
+++ lib/overlays.nix
@@ -12,6 +12,31 @@ let
   unstableOverlay = _: _: {
     unstable = import nixpkgs-unstable { inherit config system; };
   };
+  steamOverlay = _: prev: {
+    steam = prev.steam.override {
+      extraEnv = {
+        MANGOHUD = true;
+        MANGOHUD_CONFIG = prev.lib.strings.concatStringsSep "," [
+          "horizontal"
+          "battery"
+          "cpu_mhz"
+          "battery_watt"
+          "battery_time"
+          "cpu_power"
+          "gpu_power"
+          "cpu_temp"
+          "gpu_temp"
+          "device_battery=gamepad,mouse"
+          "ram"
+          "vram"
+        ];
+      };
+      extraPkgs = pkgs: with pkgs; [
+        gamescope
+        mangohud
+      ];
+    };
+  };
 in
 import nixpkgs {
   inherit config system;
@@ -19,6 +44,7 @@ import nixpkgs {
   overlays = [
     nurOverlay
     unstableOverlay
+    steamOverlay
   ];
 }
 
