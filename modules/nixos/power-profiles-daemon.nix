e3c11f87220da19b3594f9afc85680b8ac1bd492
Pierre Dal-Pra <dalpra.pierre@proton.me>
2024-05-12 13:51:31 +0200

    Backport 24.05 power-profiles-daemon module

---
+++ modules/nixos/power-profiles-daemon.nix
@@ -0,0 +1,58 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.power-profiles-daemon-custom;
+in
+
+{
+
+  ###### interface
+
+  options = {
+
+    services.power-profiles-daemon-custom = {
+
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          Whether to enable power-profiles-daemon, a DBus daemon that allows
+          changing system behavior based upon user-selected power profiles.
+        '';
+      };
+
+      package = lib.mkPackageOption pkgs "power-profiles-daemon" { };
+
+    };
+
+  };
+
+
+  ###### implementation
+
+  config = lib.mkIf cfg.enable {
+
+    assertions = [
+      {
+        assertion = !config.services.tlp.enable;
+        message = ''
+          You have set services.power-profiles-daemon-custom.enable = true;
+          which conflicts with services.tlp.enable = true;
+        '';
+      }
+    ];
+
+    environment.systemPackages = [ cfg.package ];
+
+    services = {
+      power-profiles-daemon.enable = lib.mkForce false;
+      dbus.packages = [ cfg.package ];
+      udev.packages = [ cfg.package ];
+    };
+
+    systemd.packages = [ cfg.package ];
+
+
+  };
+
+}
