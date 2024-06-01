661dc5ffde669e3ad592c433a126f8525cfcaf25
Pierre Dal-Pra <dalpra.pierre@proton.me>
2024-06-01 23:05:15 +0200

    Bump NixOS 24.05 (#118)

--- home/modules/editors/vscode.nix
+++ home/modules/editors/vscode.nix
@@ -1,21 +1,5 @@
 { pkgs, ... }:
 
-let
-  myDefaults =
-    pkgs.vscode-utils.buildVscodeExtension {
-      name = "pdalpra.vscode-defaults";
-      vscodeExtUniqueId = "vscode-defaults";
-      vscodeExtName = "My Defaults";
-      vscodeExtPublisher = "pdalpra";
-      version = "0.0.1";
-      src = pkgs.fetchFromGitHub {
-        owner = "pdalpra";
-        repo = "vscode-defaults";
-        rev = "919c61806df97badbfabaa2ee4fcbeb0343e9b27";
-        sha256 = "sha256-p2LKmNuKwXy8FTpTX+SJSLIAFyFqNIXz/FHDsK65a5I=";
-      };
-    };
-in
 {
   programs.vscode = {
     enable = true;
@@ -28,7 +12,6 @@ in
       scalameta.metals
       scala-lang.scala
       ms-vscode.theme-tomorrowkit
-      myDefaults
     ];
   };
 }
--- lib/mk-nixos.nix
+++ lib/mk-nixos.nix
@@ -35,7 +35,6 @@ lib.nixosSystem {
     disko.nixosModules.disko
     impermanence.nixosModules.impermanence
     ../modules/nixos/impermanence.nix
-    ../modules/nixos/power-profiles-daemon.nix
     ./cachix.nix
     ../system/configuration.nix
     specificConfig
