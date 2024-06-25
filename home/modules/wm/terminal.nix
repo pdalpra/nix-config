0edb9807d9c8ac7ed18ba0f2d58b5302a9c9d4af
Pierre Dal-Pra <dalpra.pierre@proton.me>
2024-06-25 22:54:53 +0200

    Fix kitty configuration

--- home/modules/wm/terminal.nix
+++ home/modules/wm/terminal.nix
@@ -6,7 +6,7 @@ _:
     extraConfig = ''
       font_family Berkeley Mono
       font_size 14
-      strip_trailing_spaces auto
+      strip_trailing_spaces smart
       enable_audio_bell no
       hide_window_decorations yes
     '';
